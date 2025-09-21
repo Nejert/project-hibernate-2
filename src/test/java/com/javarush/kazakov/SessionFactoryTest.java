988cc2d6ad85a7e6464dd6ed48a8c17c00698208
Igor Kazakov <nejer777@gmail.com>
2025-09-21 23:41:43 +0600

    Added session factory/hibernate config

---
+++ src/test/java/com/javarush/kazakov/SessionFactoryTest.java
@@ -0,0 +1,18 @@
+package com.javarush.kazakov;
+
+import org.hibernate.Session;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SessionFactoryTest {
+    @Test
+    public void getSessionAndDataTest() {
+        Integer result = null;
+        int expectedRows = 1000;
+        try (Session session = SessionFactory.getSessionFactory().openSession()) {
+            result = session.createNativeQuery("SELECT count(*) FROM film", Integer.class).uniqueResult();
+        }
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(expectedRows, result);
+    }
+}
